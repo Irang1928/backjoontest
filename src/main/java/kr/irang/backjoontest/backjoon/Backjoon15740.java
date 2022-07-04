91d3e3ae3c4b537198ee84c753333aad41a4e676
Irang <yupk1928@naver.com>
2022-07-04 14:33:32 +0900

    backjoon15740번 문제 풀기

---
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon15740.java
@@ -0,0 +1,13 @@
+package kr.irang.backjoontest.backjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Backjoon15740 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println((a.add(b)).toString());
+    }
+}
