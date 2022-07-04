f7ad73b9fb9534a950ae74f7276fcaf6b3a74af7
Irang <yupk1928@naver.com>
2022-07-04 18:26:49 +0900

    Backjoon25083

---
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon25083.java
@@ -0,0 +1,12 @@
+package kr.irang.backjoontest.backjoon;
+
+public class Backjoon25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.print("      |");
+    }
+}
