20d5460d5f98ba8b8104216c89d794d6aa2dba07
Irang <yupk1928@naver.com>
2022-07-04 17:23:12 +0900

    Backjoon1008

---
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon1008.java
@@ -0,0 +1,14 @@
+package kr.irang.backjoontest.backjoon;
+
+import java.util.Scanner;
+
+public class Backjoon1008 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        double c = (double) a / b;
+        System.out.println(c);
+    }
+}
