a5432f48ebe7ee93ed1bfdae9e23615914117bfc
Irang <yupk1928@naver.com>
2022-07-04 17:08:42 +0900

    Backjoon10171

---
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon10171.java
@@ -0,0 +1,10 @@
+package kr.irang.backjoontest.backjoon;
+
+public class Backjoon10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}
