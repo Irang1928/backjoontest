533f3566cfc61c6ffa25e248d9f9adf81565a09e
Irang <yupk1928@naver.com>
2022-07-04 18:23:55 +0900

    Backjoon2588

---
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon2588.java
@@ -0,0 +1,16 @@
+package kr.irang.backjoontest.backjoon;
+
+import java.util.Scanner;
+
+public class Backjoon2588 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a * (b % 10));
+        System.out.println(a * ((b % 100) / 10));
+        System.out.println(a * (b / 100));
+        System.out.println(a * b);
+    }
+}
