7aba92ef9eb4573ca21823b8777fc72427f68753
Irang <yupk1928@naver.com>
2022-07-05 10:53:42 +0900

    Backjoon14681

---
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon14681.java
@@ -0,0 +1,24 @@
+package kr.irang.backjoontest.backjoon;
+
+import java.util.Scanner;
+
+public class Backjoon14681 {
+    public static void main(String[] args) {
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+        if (x > 0 && y > 0) {
+            System.out.print("1");
+        }
+        else if (x < 0 && y > 0) {
+            System.out.print("2");
+        }
+        else if (x < 0 && y < 0) {
+            System.out.print("3");
+        }
+        else if (x > 0 && y < 0) {
+            System.out.print("4");
+        }
+    }
+}
