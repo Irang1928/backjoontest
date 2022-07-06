1e273139c0b49a2efa84b95cdc60f1242c2cfce3
Irang <yupk1928@naver.com>
2022-07-06 15:35:48 +0900

    Backjoon2741

--- src/main/java/kr/irang/backjoontest/backjoon/Backjoon15552.java
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon15552.java
@@ -9,12 +9,12 @@ public class Backjoon15552 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int t = Integer.parseInt(br.readLine());
         for (int i = 0;i < t;i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            bw.write(a + b + "\n");
+            StringTokenizer st = new StringTokenizer(br.readLine());    // 입력 받기
+            int a = Integer.parseInt(st.nextToken());   // 첫번째 입력받은 숫자 넣기
+            int b = Integer.parseInt(st.nextToken());   // 두번째 입력받은 숫자 넣기
+            bw.write(a + b + "\n");     // 출력
         }
-        bw.flush();
+        bw.flush();     // 스트림 비우기
         bw.close();
     }
 }
