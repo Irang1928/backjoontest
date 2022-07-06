76e37fa2b7b7b5fd3bdaab80021476eec8283749
Irang <yupk1928@naver.com>
2022-07-06 16:54:55 +0900

    Backjoon2438

---
+++ src/main/java/kr/irang/backjoontest/backjoon/Backjoon2438.java
@@ -0,0 +1,19 @@
+package kr.irang.backjoontest.backjoon;
+
+import java.io.*;
+
+public class Backjoon2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0;i < n;i++) {
+            for (int j = 0; j <= i;j++) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
